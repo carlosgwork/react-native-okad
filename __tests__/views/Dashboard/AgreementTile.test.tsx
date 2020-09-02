d5b3b88e14c0a38db47d79bca881f57bb76a882a
solidperez <hitgoal312@gmail.com>
2020-09-02 13:06:04 -0400

    Update agreement tile unit tests

--- __tests__/views/Dashboard/AgreementTile.test.tsx
+++ __tests__/views/Dashboard/AgreementTile.test.tsx
@@ -4,10 +4,13 @@
 
 import React from 'react';
 import {mount, ReactWrapper} from 'enzyme';
+import wait from 'waait';
+
 import {ThemeContext, ThemeContextType} from '@global/Context';
 import getThemeStyle from '@root/utils/styles';
 import AgreementTile from '@root/views/Dashboard/AgreementTile';
 import {TILE_MOCKDATA} from '../../__mocks__/Dashboard';
+import {TouchElementProps} from '@root/utils/types';
 
 let wrapper: ReactWrapper;
 
@@ -17,22 +20,38 @@ const currentTheme = {
   theme,
   themeStyle: getThemeStyle(theme),
 } as ThemeContextType;
+const jestMockCallback = jest.fn();
 
 describe('Agreement Component in Dashboard page', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     wrapper = mount(
       <ThemeContext.Provider value={currentTheme}>
         <AgreementTile
+          onPress={jestMockCallback}
           agreement={TILE_MOCKDATA.agreement as any}
           color={TILE_MOCKDATA.color}
         />
       </ThemeContext.Provider>,
     );
+    await wait(0);
   });
 
   it('renders successfully', () => {
-    const textEle = wrapper.find('Memo(AppText)');
-    expect(textEle.text()).toBe('Linnie Lindgren (Harmar Straight Stairlift)');
-    expect(textEle.prop('style')).toEqual({color: TILE_MOCKDATA.color});
+    wrapper.update();
+    expect(wrapper.find('Memo(AppText)').at(0).text()).toEqual(
+      'Linnie Lindgren  ',
+    );
+    expect(
+      wrapper.find('Text').contains('(Bruno Straight Stairlift)'),
+    ).toBeTruthy();
+    expect(wrapper.find('AgreementTile').prop('color')).toEqual(
+      TILE_MOCKDATA.color,
+    );
+  });
+
+  it('should fire a touch event when clicked', () => {
+    const button = wrapper.find('ForwardRef');
+    (button.at(0).props() as TouchElementProps).onPress();
+    expect(jestMockCallback).toBeCalled();
   });
 });
